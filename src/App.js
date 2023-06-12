6ae0b623dfec85c1da879e59a789cdbbf0f53466
Prajwalg19 <theimerpical8@gmail.com>
2023-06-13 00:39:40 +0530

    added navigation bar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import { BrowserRouter as Bingo, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import SignOut from "./pages/SignOut";
 import Offers from "./pages/Offers";
 import ForgotPassword from "./pages/ForgotPassword";
-
+import Header from "./components/Header";
 function App() {
     return (
-        <Bingo>
-            <Routes>
-                <Route path="/" element={<Home />}></Route>
-                <Route path="/profile" element={<Profile />}></Route>
-                <Route path="/sign-in" element={<SignIn />}></Route>
-                <Route path="/sign-out" element={<SignOut />}></Route>
-                <Route path="/forgot-password" element={<ForgotPassword />}></Route>
-                <Route path="/offers" element={<Offers />}></Route>
-            </Routes>
-        </Bingo>
+        <>
+            <Router>
+                <Header />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/profile" element={<Profile />} />
+                    <Route path="/sign-in" element={<SignIn />} />
+                    <Route path="/sign-out" element={<SignOut />} />
+                    <Route path="/forgot-password" element={<ForgotPassword />} />
+                    <Route path="/offers" element={<Offers />} />
+                </Routes>
+            </Router>
+        </>
     );
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,54 @@
+import { useNavigate } from "react-router-dom";
+import { useLocation } from "react-router";
+function Header() {
+    const location = useLocation();
+    const navigate = useNavigate();
+    function change(params) {
+        if (params == location.pathname) return true;
+    }
+    const navigation = (value) => {
+        navigate(value);
+    };
+    return (
+        <>
+            <div className="sticky top-0 bg-white border-b shadow-md z-4">
+                <header className="flex items-center justify-between  mx-auto max-w-[1400px]">
+                    <div>
+                        <img src="https://cdn.worldvectorlogo.com/logos/flipkart.svg" onClick={() => navigation("/")} alt="Flipkart Logo" className="h-8 cursor-pointer" />
+                    </div>
+                    <div>
+                        <ul className="flex ; space-x-9 ">
+                            <li
+                                className={` py-4 cursor-pointer text-gray-400  border-b-4 text-sm font-semibold border-transparent ${change("/") && "text-black border-b-red-500"}`}
+                                onClick={() => {
+                                    navigation("/");
+                                }}
+                            >
+                                {" "}
+                                Home{" "}
+                            </li>
+
+                            <li
+                                className={`py-4 cursor-pointer text-gray-400  border-b-4 text-sm font-semibold border-transparent ${change("/offers") && "text-black border-b-red-500"}`}
+                                onClick={() => {
+                                    navigation("/offers");
+                                }}
+                            >
+                                Offers
+                            </li>
+                            <li
+                                className={`py-4 cursor-pointer text-gray-400  border-b-4 text-sm font-semibold border-transparent ${change("/sign-in") && "text-black border-b-red-500"}`}
+                                onClick={() => {
+                                    navigation("/sign-in");
+                                }}
+                            >
+                                Sign In
+                            </li>
+                        </ul>
+                    </div>
+                </header>
+            </div>
+        </>
+    );
+}
+export default Header;
