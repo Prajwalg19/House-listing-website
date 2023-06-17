02383d1594454c68e9f48f40963037f0a1f54e7f
Prajwalg19 <theimerpical8@gmail.com>
2023-06-18 01:21:21 +0530

    sign up page almost done

---
+++ src/components/googleButton.jsx
@@ -0,0 +1,7 @@
+export function But() {
+    return (
+        <div>
+            <button className="w-full py-3 text-sm font-semibold text-white uppercase bg-red-600 shadow hover:bg-red-700 duration-100 ease-in-out hover:shadow-lg active:bg-red-900">Continue with google</button>
+        </div>
+    );
+}
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,33 +1,52 @@
 import { useState } from "react";
-
+import { AiFillEyeInvisible } from "react-icons/ai";
+import { AiFillEye } from "react-icons/ai";
+import { Link } from "react-router-dom";
+import { Button } from "../components/googleButton";
 function SignIn() {
+    const [showPassword, setShowPassword] = useState(false);
     const [formData, setFormData] = useState({
         email: "",
         password: "",
     });
-
+    console.log("new state ", formData);
     const { email, password } = formData;
-    function updateEmail(event) {
-        const temp = event.target.value;
-        // let mail = formData.email;
-        // setFormData((mail = temp));
-        // console.log(formData.email);
-        let mail = { ...formData };
-        mail.email = temp;
-        setFormData(mail);
-        console.log(formData.email);
+    function update(event) {
+        setFormData((prev) => ({ ...prev, [event.target.id]: event.target.value }));
     }
     return (
         <div>
             <h1 className="font-bold text-3xl text-center mt-[30px]">Sign In </h1>
-            <div className="flex flex-wrap items-center justify-center max-w-6xl m-auto py-14 ">
-                <div className="md:w-[67%] lg:w-[47%] mb-[80px] sm:mb-[80px]">
+            <div className="flex flex-wrap items-center justify-center   max-w-6xl m-auto py-14 ">
+                <div className="md:w-[67%] lg:w-[47%] w-[70%] mb-[80px] ">
                     <img src="https://img.freepik.com/premium-vector/computer-account-login-password_165488-5473.jpg" className="w-full rounded-2xl" alt="Image of Sign in" />
                 </div>
-                <div className="md:w-[67%] lg:w-[40%]  w-full lg:ml-[75px]">
-                    <form name="">
-                        <input type="email" placeholder="Email address" className="w-full pl-4 mb-4" value={email} onChange={updateEmail} /> <br />
-                        <input type="password" placeholder="Password" className="w-full pl-4 " />
+                <div className="md:w-[67%] lg:w-[40%]  w-[70%] lg:ml-[75px] ">
+                    <form>
+                        <input type="email" placeholder="Email address" className="w-full pl-4 mb-4 text-xl text-gray-700 rounded-lg transition ease-in ease-out" value={email} id="email" onChange={update} /> <br />
+                        <div className="relative">
+                            <input type={showPassword ? "text" : "password"} placeholder="Password" className="w-full pl-4 text-xl border-gray-500 rounded-lg " id="password" value={password} onChange={update} />
+                            {showPassword ? <AiFillEyeInvisible className="cursor-pointer absolute top-3 right-3" onClick={() => setShowPassword((pre) => !pre)} /> : <AiFillEye className="cursor-pointer absolute top-3 right-3" onClick={() => setShowPassword((prev) => !prev)} />}
+                        </div>
+                        <div className="flex mt-4 justify-between whitespace-nowrap text-sm lg:text-lg">
+                            <p>
+                                Don't have an account?{"  "}
+                                <Link to="/sign-up" className="ml-1 duration-200  ease-in-out hover:text-red-700 cursor-pointer inline-block text-red-600">
+                                    Register
+                                </Link>
+                            </p>
+                            <p className="text-blue-600 cursor-pointer hover:text-blue-700">
+                                <Link to={"/forgot-password"}>Forgot Password? </Link>
+                            </p>
+                        </div>
+                        <div>
+                            <button type="submit" className="px-44 bg-blue-600 w-full text-white rounded-sm py-3 mt-3 text-sm font-semibold uppercase shadow hover:bg-blue-700 duration-150 hover:shadow-lg active:bg-blue-800   ">
+                                {" "}
+                                Sign in
+                            </button>
+                            <p className="text-center my-3">OR</p>
+                            <Button />
+                        </div>
                     </form>
                 </div>
             </div>
