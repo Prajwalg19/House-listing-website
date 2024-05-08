451fbc90c245a4b9d735677917729e4e52992a85
Prajwal G <93521144+Prajwalg19@users.noreply.github.com>
2024-05-08 10:54:31 +0530

    Update ListingItems.jsx

--- src/components/ListingItems.jsx
+++ src/components/ListingItems.jsx
@@ -9,11 +9,11 @@ function ListingItems(prop) {
         <>
             {listing.length > 0 && (
                 <div className="max-w-7xl mx-auto ">
-                    <ul className="sm:grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 ">
+                    <ul className="flex sm:flex-col space-between flex-wrap">
                         {listing.map((l) => {
                             let list = l.data;
                             return (
-                                <li key={l.id} className=" mb-8 z-0 relative bg-white w-full  shadow-md hover:shadow-xl rounded-md overflow-hidden transition-shadow duration-150 m-[10px]">
+                                <li key={l.id} className=" mb-8 z-0 relative bg-white w-[300px] shadow-md hover:shadow-xl rounded-md overflow-hidden transition-shadow duration-150 m-[10px]">
                                     <Link to={`/category/${list.sellOrRent}/${l.id}`} className="contents">
                                         <img src={`${list.imgUrls[0]}`} className="h-[300px] w-full object-cover hover:scale-105 transition ease-in-out duration-150" loading="lazy" />
                                         <Moment fromNow className="shadow-lg font-semibold text-xs uppercase top-3 rounded left-3 text-white px-2 py-1 bg-purple-600 absolute z-1">
